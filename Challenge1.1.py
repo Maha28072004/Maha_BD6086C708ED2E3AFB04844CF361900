aaa3a8b4c3f963b7288ef7560e6a4fa65337610a
Maha28072004 <146057350+Maha28072004@users.noreply.github.com>
2023-09-27 15:27:33 +0530

    Add files via upload

---
+++ Challenge1.1.py
@@ -0,0 +1,9 @@
+#Implementing a recursive function to find the factorial of a number
+def recursive_fact(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*recursive_fact(n-1)
+number=2
+result=recursive_fact(number)
+print("The factorial of {} is {}".format(number,result))
